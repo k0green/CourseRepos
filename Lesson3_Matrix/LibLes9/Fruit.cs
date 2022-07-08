ac8d29e654decba7e4af01953862b8028f01e5ce
k0green <novikegor3@gmail.com>
2022-07-08 22:09:36 +0300

    HW9

---
+++ Lesson3_Matrix/LibLes9/Fruit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public struct Fruit
+    {
+
+        public string Name { get; set; }
+        internal double Coast { get; set; }
+        private int Id { get; set; }
+        public Fruit(string name, double coast, int id):this()
+        {
+            Name = name;
+            Coast = coast;
+            Id = id;
+        }
+
+        public bool Display()
+        {
+            Console.WriteLine($"Fruit: {Name} coast: {Coast} id:{Id}");
+            return true;
+        }
+    }
+}
---
+++ Lesson3_Matrix/LibLes9/Vegetable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8
+{
+    public class Vegetable
+    {
+
+        private string Name { get; set; }
+        public double Coast { get; set; }
+        private int Id { get; set; }
+        public Vegetable(string name, double coast, int id)
+        {
+            Name = name;
+            Coast = coast;
+            Id= id;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public bool Print()
+        {
+            Console.WriteLine($"Vegetable: {Name} coast: {Coast} id:{Id}");
+            return true;
+        }
+    }
+}
