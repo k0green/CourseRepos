83e142103a2acee740bbddc5590b5da0f3cd7ace
k0green <novikegor3@gmail.com>
2022-08-22 20:52:13 +0300

    HW17 Again

---
+++ Lesson3_Matrix/Lesson17/CarsModels/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson15_CarCompany.Models
+{
+    [Serializable]
+    internal class Car : ICarTemplate, ICloneable, IComparable<Car>
+    {
+        public string Company { get; set; }
+        public string Model { get; set; }
+        public int Id { get; set; }
+        public int Year { get; set; }
+
+        public Car()
+        {
+            Company = "Toyota";
+            Model = "Camry";
+            Id = 1984;
+            Year = 2009;
+        }
+
+        public object Clone() => MemberwiseClone();
+        public void GetInfo()
+        {
+            Console.WriteLine($"Car: Company: {Company} - Model: {Model} - Year: {Year} - Id: {Id}");
+        }
+
+        public int CompareTo(Car? other)
+        {
+            if (other is Car car) return Company.CompareTo(car.Company);
+            else throw new ArgumentException("Некорректное значение параметра");
+        }
+    }
+}
