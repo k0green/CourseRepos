aa746de0fceafd2721e893c0ff85140e3ea5b62d
Егор Новик <stud1070242012@bntu.by>
2022-10-12 12:31:07 +0300

    HW31

---
+++ Lesson3_Matrix/Lesson31/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace Lesson31.Models
+{
+    public class Person : IDisposable
+    {
+        public string Name { get; set; }
+
+        public Person(string name) => Name = name;
+        ~Person()
+        {
+            Console.WriteLine($"{Name} was deleted");
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine($"{Name} was disposed");
+        }
+    }
+}
---
+++ Lesson3_Matrix/Lesson31/Program.cs
@@ -0,0 +1,12 @@
+﻿using Lesson31.Models;
+
+Test();
+GC.Collect();
+Console.ReadLine();
+
+void Test()
+{
+    Person mike = new Person("Mike");
+    mike.Dispose();
+
+}
