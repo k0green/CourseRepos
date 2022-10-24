899c7cec2830f8e4e8dc02114dea669a92ad5ac9
Егор Новик <stud1070242012@bntu.by>
2022-10-24 17:55:24 +0300

    Add using

--- Lesson3_Matrix/Lesson31/Program.cs
+++ Lesson3_Matrix/Lesson31/Program.cs
@@ -8,5 +8,9 @@ void Test()
 {
     Person mike = new Person("Mike");
     mike.Dispose();
+    using (Person nik = new Person("Nik"))
+    {
+        Console.WriteLine($"Name: {nik.Name}");
+    }
 
 }
